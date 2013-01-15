c7f4c1ee056c52d184728caee57104b271f78feb
Donagh Hatton <donagh@donagh-laptop.(none)>
2013-01-15 23:18:11 +0000

    Consts changed to C++ style

--- src/titan/Consts.h
+++ src/titan/Consts.h
@@ -1,27 +1,22 @@
-typedef enum
+enum Motor
 {
     MOTOR_LEFT, MOTOR_RIGHT
-}
-motor_t;
+};
 
-typedef enum
+enum IR
 {
     IR_FRONT_LEFT, IR_FRONT_RIGHT, IR_BOTTOM_LEFT, IR_BOTTOM_RIGHT
-}
-ir_t;
+};
 
-typedef enum
+enum Echo
 {
     ECHO_FRONT = 0, ECHO_BACK = 1
-}
-echo_t;
+};
 
-typedef enum
+enum Pins
 {
     ECHO_FRONT_TRIGGER = 4,
     ECHO_FRONT_ECHO = 5,
     ECHO_BACK_TRIGGER = 8,
     ECHO_BACK_ECHO = 9
-}
-pin_t;
-
+};
