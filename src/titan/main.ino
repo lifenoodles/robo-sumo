e71f9861a0697eb6e815ed5703b758695fcd72ab
Donagh Hatton <donagh@donagh-laptop.(none)>
2013-01-15 14:13:43 +0000

    consts added

--- src/titan/main.ino
+++ src/titan/main.ino
@@ -1,3 +1,4 @@
+#include "../lib/NewPing.h"
 #include "titan_consts.h"
 
 void setup()
---
+++ src/titan/titan_consts.h
@@ -0,0 +1,23 @@
+typedef enum
+{
+    MOTOR_LEFT, MOTOR_RIGHT
+}
+motor_t;
+
+typedef enum
+{
+    IR_FRONT_LEFT, IR_FRONT_RIGHT, IR_BOTTOM_LEFT, IR_BOTTOM_RIGHT
+}
+ir_t;
+
+typedef enum
+{
+    ECHO_FRONT, ECHO_BACK
+}
+echo_t;
+
+typedef enum
+{
+    PIN_MOTOR_LEFT_A
+}
+pin_t;
