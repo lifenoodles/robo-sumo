8917162a07cd50614ca876f3455d98cd2ca55f97
Donagh Hatton <donagh@donagh-laptop.(none)>
2013-01-17 12:07:55 +0000

    main loop updated

--- src/Sumo.ino
+++ src/Sumo.ino
@@ -3,13 +3,13 @@
 #include "Motors.h"
 #include "Consts.h"
 #include "BlueTooth.h"
+#include "Behaviour.h"
 #include <EEPROM.h>
 
 SensorsEcho sensorsEcho;
 
 void setup()
 {
-    EEPROM.write(ADDRESS_ID, ID_PHOBOS);
     char id = EEPROM.read(ADDRESS_ID);
     Serial.begin(9600);
     Serial.print("ID: ");
@@ -27,16 +27,7 @@ void loop()
 {
     int timePassed = millis();
     timer += timePassed;
-    if(timer > 50)
-    {
-        BlueTooth::get()->report();
-        timer = 0;
-    }
     Sensors::get()->update(timePassed);
-   /* Serial.println(Sensors::get()->ir->getValue(IR_BACK_LEFT), DEC);
-    Serial.print("\t\t");
-    Serial.println(Sensors::get()->ir->getValue(IR_BACK_RIGHT), DEC);*/
-    //int x = 5;
-    //Serial.println(x);
-    //Serial.println(5);
+    Behaviour::get()->update(timePassed);
+    BlueTooth::get()->report(timePassed);
 }
